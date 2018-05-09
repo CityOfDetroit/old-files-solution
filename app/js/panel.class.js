3b6d5423bd5b9d112df3ab7f6dbba7d79b20aeba
J.Edgar Montes <montesje@ds.detroitmi.gov>
2018-05-09 18:02:02 -0400

    adding template for emergency manager orders

--- app/js/panel.class.js
+++ app/js/panel.class.js
@@ -23,11 +23,20 @@ export default class Panel {
         return `<span class="red-text">${value}</span>`;
         break;
       default:
-
+        return ``;
     }
   }
+  getFileLink(value, baseURL){
+    let tempHTML = null;
+    for (var key in value.doc) {
+      if (value.doc.hasOwnProperty(key)) {
+        tempHTML = `<a href="${baseURL}/${value.doc[key]}" target="_blank">${key}</a>`
+      }
+    }
+    return tempHTML;
+  }
   createMarkup(values, controller){
-    console.log(values);
+    // console.log(values);
     let tempHTML = '';
     let params = controller.panel.version.split('--');
     switch (params[0]) {
@@ -58,6 +67,38 @@ export default class Panel {
               </section>`
             ).join('')}
             `;
+            break;
+          default:
+
+        }
+        break;
+      case 'archive':
+        switch (params[1]) {
+          case 'v01':
+            let index = 0;
+            Object.entries(values.reports).forEach(([key, value]) => {tempHTML += `
+            <div class="card">
+              <div class="card-header" id="h-${index}">
+                <h5 class="mb-0">
+                  <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#c-${index}" aria-expanded="true" aria-controls="c-${index}">
+                    ${key}
+                  </button>
+                </h5>
+              </div>
+
+              <div id="c-${index}" class="collapse" aria-labelledby="h-${index}">
+                <div class="card-body">
+                  <ul>
+                    ${value.map(doc => `
+                      <li>${controller.panel.getFileLink({doc}, values.base_url)}</li>
+                    `).join('')}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            `;
+            index++;
+            });
             break;
           default:
 
